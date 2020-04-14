abbdffe22ee0485ea2ca6542783031da866816d8
FJQACR <963555083@qq.com>
2020-04-14 21:12:38 +0800

    create project

---
+++ src/com/ifish/servlet/home/helloTest.java
@@ -0,0 +1,36 @@
+package com.ifish.servlet.home;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.ifish.utils.CheckUtil;
+
+public class helloTest extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String signature = req.getParameter("signature");
+		String timestamp = req.getParameter("timestamp");
+		String nonce = req.getParameter("nonce");
+		String echostr = req.getParameter("echostr");
+
+		PrintWriter pWriter = resp.getWriter();
+		if (CheckUtil.checkSignature(signature, timestamp, nonce)) {
+			pWriter.print(echostr);
+		}
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doPost(req, resp);
+	}
+	
+	
+
+}
